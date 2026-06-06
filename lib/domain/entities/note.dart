3340a30d9f427835522bb8996cf9d92c574c95d5
Vicienna <108596016+Vicienna@users.noreply.github.com>
2026-06-07 03:27:11 +0700

    Hahaha

---
+++ lib/domain/entities/note.dart
@@ -0,0 +1,33 @@
+class Note {
+  final int? id;
+  final String title;
+  final String content;
+  final DateTime createdAt;
+
+  Note({
+    this.id,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+  });
+
+  // Untuk konversi dari Map (Database) ke Object
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'],
+      content: map['content'],
+      createdAt: DateTime.parse(map['createdAt']),
+    );
+  }
+
+  // Untuk konversi dari Object ke Map (Database)
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+}
