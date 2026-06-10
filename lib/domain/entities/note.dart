9dd808a8a710e5baf54a2ce544de74e68e807b55
Vicienna <108596016+Vicienna@users.noreply.github.com>
2026-06-10 15:58:13 +0700

    Pushing files via CLI

--- lib/domain/entities/note.dart
+++ lib/domain/entities/note.dart
@@ -3,31 +3,55 @@ class Note {
   final String title;
   final String content;
   final DateTime createdAt;
+  final int color;
+  final String category;
 
   Note({
     this.id,
     required this.title,
     required this.content,
     required this.createdAt,
+    this.color = 0xFFFFFFFF,
+    this.category = 'Umum',
   });
 
-  // Untuk konversi dari Map (Database) ke Object
   factory Note.fromMap(Map<String, dynamic> map) {
     return Note(
       id: map['id'],
       title: map['title'] ?? 'Tanpa Judul',
       content: map['content'] ?? '',
       createdAt: DateTime.tryParse(map['createdAt'] ?? '') ?? DateTime.now(),
+      color: map['color'] ?? 0xFFFFFFFF,
+      category: map['category'] ?? 'Umum',
     );
   }
 
-  // Untuk konversi dari Object ke Map (Database)
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'title': title,
       'content': content,
       'createdAt': createdAt.toIso8601String(),
+      'color': color,
+      'category': category,
     };
+  }
+
+  Note copyWith({
+    int? id,
+    String? title,
+    String? content,
+    DateTime? createdAt,
+    int? color,
+    String? category,
+  }) {
+    return Note(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      content: content ?? this.content,
+      createdAt: createdAt ?? this.createdAt,
+      color: color ?? this.color,
+      category: category ?? this.category,
+    );
   }
 }
